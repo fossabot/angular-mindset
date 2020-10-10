0af920eac8d502acb0eaadfabdbdb00833b62504
David Dembinski <darkvplayer@gmail.com>
2020-10-10 03:49:04 +0200

    Added fake backend.

--- src/app/interceptors/fake-backend.interceptor.ts
+++ src/app/interceptors/fake-backend.interceptor.ts
@@ -1,3 +1,4 @@
+import { Task } from './../models/Task';
 import { Injectable } from '@angular/core';
 import {
   HttpRequest,
@@ -37,9 +38,37 @@ function ok<T>(body: T | null, status = 200) {
   return of(new HttpResponse({status, body}))
 }
 
+function task(id: number): Observable<HttpEvent<Task>> {
+  if (id === 4592) {
+    return ok({
+      title: "Erstellung einer App für Gedankensortierung",
+      id: 4592,
+      questions: [
+        {
+          content: "Was wird für die App aktuell benötigt?",
+          answers: [
+            { content: "Einen Konfigurator." },
+            { content: "Eine UI in der man neue Fragestellungen und Aufgaben definieren kann."}
+          ]
+        }
+      ]
+    })
+  }
+
+  return ok({
+    title: "",
+    id: -1,
+    questions: []
+  })
+}
+
 function handleRoute({url, method}: HttpRequest<any>): Observable<HttpEvent<any>> | null {
   if (url.endsWith('/test') && method === METHOD.GET) {
     return test();
+  }
+
+  if (url.endsWith('/task/1') && method === METHOD.GET) {
+    return task(4592);
   }
 
   return null;
