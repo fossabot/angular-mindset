a2b2a05bfcbed9b81b550f4ba9383089d7bde00b
David Dembinski <darkvplayer@gmail.com>
2020-10-10 03:51:35 +0200

    Added simple tasks view component.

--- src/app/components/task-link/task-link.component.ts
+++ src/app/components/task-link/task-link.component.ts
@@ -1,4 +1,6 @@
+import { TaskService } from './../../services/task.service';
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/Task';
 
 @Component({
   selector: 'app-task-link',
@@ -7,9 +9,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TaskLinkComponent implements OnInit {
 
-  constructor() { }
+  taskId = -1;
+  questionId = -1;
+  tasks: Array<Task>
+
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTask(1).subscribe((t: Task) => {
+      if (t) {
+        this.tasks.push(t);
+      }
+    });
   }
 
+  getTaskTitles() {
+    return this.tasks.map(t => t.title);
+  }
+
+  getQuestionsContent() {
+    return this.tasks[this.taskId].questions.map(q => q.content);
+  }
+
+  getAnswersContent() {
+    return this.tasks[this.taskId].questions[this.questionId].answers.map(a => a.content);
+  }
+
+  selectTask() {
+
+  }
+
+  selectQuestion() {
+
+  }
 }
