da188c12ef6513914772ef05d3cd65cd373a110c
David Dembinski <darkvplayer@gmail.com>
2020-10-10 03:50:07 +0200

    Added simple dropdown field.

--- src/app/components/task-link-items/task-link-item-dropdown/task-link-item-dropdown.component.ts
+++ src/app/components/task-link-items/task-link-item-dropdown/task-link-item-dropdown.component.ts
@@ -1,13 +1,30 @@
+import { trigger, transition, animate, state, style } from '@angular/animations';
+import { EventEmitter, Input, Output } from '@angular/core';
 import { Component, OnInit } from '@angular/core';
+import { OuterSubscriber } from 'rxjs/internal/OuterSubscriber';
 
 @Component({
   selector: 'app-task-link-item-dropdown',
   templateUrl: './task-link-item-dropdown.component.html',
-  styleUrls: ['./task-link-item-dropdown.component.css']
+  styleUrls: ['./task-link-item-dropdown.component.css'],
+  animations: [
+    trigger('dropdown', [
+      state('void', style({ height: 0 })),
+      state('*', style({ height: '*' })),
+      transition(':enter', animate('0.5s ease')),
+      transition(':leave', animate('0.5s ease'))
+    ])
+  ]
 })
 export class TaskLinkItemDropdownComponent implements OnInit {
 
-  flagDropdown = false;
+  @Input() values = [];
+  @Input() hint = 'Selection...';
+
+  @Output() selected = new EventEmitter();
+
+  selection = '';
+  showDropdown = false;
 
   constructor() { }
 
@@ -15,7 +32,11 @@ export class TaskLinkItemDropdownComponent implements OnInit {
   }
 
   toggleDropdown() {
-    console.log(this.flagDropdown);
-    this.flagDropdown = !this.flagDropdown;
+    console.log(this.showDropdown);
+    this.showDropdown = !this.showDropdown;
+  }
+
+  emitClickedIndex(value: string) {
+    this.selected.next(this.values.indexOf(value));
   }
 }
